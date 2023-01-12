033ed492765229276c401171a414753166c9eee0
Yousef <yousefelsisyy@gmail.com>
2023-01-12 13:44:29 -0400

    Create addBook button and link it to JS

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //An empty array to store all the books in.
 let myLibrary = []
 
+function main() {
+    addBookButton();
+}
+
 //Constructor for book objects.
 function Book(author, title, pagesNum, read) {
     this.author = author;
@@ -15,3 +19,18 @@ function addBookToLibrary(author, title, pagesNum, read) {
     let newBook = new Book(author, title, pagesNum, read);
     myLibrary.push(newBook);
 }
+
+//Function adds interactivity to "addBook" button.
+function addBookButton() {
+    button = document.querySelector(".addBook");
+    button.addEventListener("click", createForm);
+}
+
+//This function activates when a button is pressed to add
+//a book. It will create a form for the user to input
+//details of their book.
+function createForm() {
+    console.log("Works");
+}
+
+main();
