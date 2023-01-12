f353481800e18f74900419bf9213ee64280dbd7b
Yousef <yousefelsisyy@gmail.com>
2023-01-12 12:10:57 -0400

    Create skeleton JS for code organization

---
+++ script.js
@@ -0,0 +1,13 @@
+//An empty array to store all the books in.
+let myLibrary = []
+
+//Constructor for book objects.
+function Book() {
+    //Stuff
+}
+
+//Function that uses user input to create a book object and then add it to
+//the myLibrary array.
+function addBookToLibrary() {
+    //Stuff
+}
